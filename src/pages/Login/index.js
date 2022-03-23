6e108d64dd377b7be431c6816be34802b528d268
helmyandria <helmyandria@gmail.com>
2022-03-23 11:39:09 +0700

    add lib redux.js, lib react-redux, implement redux on loading

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import {ILLogo} from '../../assets';
-import {Input, Link, Button, Gap, Loading} from '../../components';
+import {Input, Link, Button, Gap} from '../../components';
 import {colors, fonts, useForm, storeData} from '../../utils';
 import {Fire} from '../../config';
 import {showMessage} from 'react-native-flash-message';
+import {useDispatch} from 'react-redux';
 
 const Login = ({navigation}) => {
   const [form, setForm] = useForm({email: '', password: ''});
-  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
   const login = () => {
     console.log('form: ', form);
-    setLoading(true);
+    dispatch({type: 'SET_LOADING', value: true});
     Fire.auth()
       .signInWithEmailAndPassword(form.email, form.password)
       .then(res => {
         console.log('success: ', res);
-        setLoading(false);
+        dispatch({type: 'SET_LOADING', value: false});
         Fire.database()
           .ref(`users/${res.user.uid}/`)
           .once('value')
@@ -31,7 +32,7 @@ const Login = ({navigation}) => {
       })
       .catch(err => {
         console.log('error: ', err);
-        setLoading(false);
+        dispatch({type: 'SET_LOADING', value: false});
         showMessage({
           message: err.message,
           type: 'default',
@@ -40,40 +41,38 @@ const Login = ({navigation}) => {
         });
       });
   };
+
   return (
-    <>
-      <View style={styles.page}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Gap height={40} />
-          <ILLogo />
-          <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
-          <Input
-            label="Email Address"
-            value={form.email}
-            onChangeText={value => setForm('email', value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Password"
-            value={form.password}
-            onChangeText={value => setForm('password', value)}
-            secureTextEntry
-          />
-          <Gap height={10} />
-          <Link title="Forgot My Password" size={12} />
-          <Gap height={40} />
-          <Button title="Sign In" onPress={login} />
-          <Gap height={30} />
-          <Link
-            title="Create New Account"
-            size={16}
-            align="center"
-            onPress={() => navigation.navigate('Register')}
-          />
-        </ScrollView>
-      </View>
-      {loading && <Loading />}
-    </>
+    <View style={styles.page}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Gap height={40} />
+        <ILLogo />
+        <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+        <Input
+          label="Email Address"
+          value={form.email}
+          onChangeText={value => setForm('email', value)}
+        />
+        <Gap height={24} />
+        <Input
+          label="Password"
+          value={form.password}
+          onChangeText={value => setForm('password', value)}
+          secureTextEntry
+        />
+        <Gap height={10} />
+        <Link title="Forgot My Password" size={12} />
+        <Gap height={40} />
+        <Button title="Sign In" onPress={login} />
+        <Gap height={30} />
+        <Link
+          title="Create New Account"
+          size={16}
+          align="center"
+          onPress={() => navigation.navigate('Register')}
+        />
+      </ScrollView>
+    </View>
   );
 };
 
