3938d1ff7971cb42c71e718c68478c3fe196a9de
helmyandria <helmyandria@gmail.com>
2022-04-05 12:53:32 +0700

    send data chat to firebase

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,10 +1,53 @@
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ChatItem, Header, InputChat} from '../../components';
-import {colors, fonts} from '../../utils';
+import {colors, fonts, getData, showError} from '../../utils';
+import {Fire} from '../../config';
 
 const Chatting = ({navigation, route}) => {
   const dataDoctor = route.params;
+  const [chatContent, setChatContent] = useState('');
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    getData('user').then(res => {
+      console.log('user login: ', res);
+      setUser(res);
+    });
+  }, []);
+
+  const chatSend = () => {
+    console.log('chat Send Press', chatContent);
+    const today = new Date();
+    const hour = today.getHours();
+    const minute = today.getMinutes();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    const data = {
+      sendBy: user.uid,
+      chatDate: new Date().getTime(),
+      chatTime: `${hour}:${minute} ${hour > 12 ? 'PM' : 'AM'}`,
+      chatContent: chatContent,
+    };
+    console.log('data utk di kirim: ', data);
+    console.log(
+      'url firebase: ',
+      `chatting/${user.uid}_${dataDoctor.data.uid}/allChat/${year}-${month}-${date}`,
+    );
+
+    Fire.database()
+      .ref(
+        `chatting/${user.uid}_${dataDoctor.data.uid}/allChat/${year}-${month}-${date}`,
+      )
+      .push(data)
+      .then(() => {
+        setChatContent('');
+      })
+      .catch(err => {
+        showError(err.message);
+      });
+  };
   return (
     <View style={styles.page}>
       <Header
@@ -23,9 +66,9 @@ const Chatting = ({navigation, route}) => {
         </ScrollView>
       </View>
       <InputChat
-        value=""
-        onChangeText={() => alert('input tap')}
-        onButtonPress={() => alert('button press')}
+        value={chatContent}
+        onChangeText={value => setChatContent(value)}
+        onButtonPress={chatSend}
       />
     </View>
   );
